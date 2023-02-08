79f7e68db5ae30b2d27ec9574f38cd45aa0c2493
Nilesh <Nilesh@DESKTOP-PGNCCBS>
2023-02-08 20:02:30 +0530

    This is the secondProject to be share

---
+++ javaProject/src/first/Test.java
@@ -0,0 +1,15 @@
+package first;
+
+public class Test {
+
+	public static void main(String[] args) {
+		int a=20;
+		int b=30;
+		a=a+b;
+		b=a-b;
+		a=a-b;
+		System.out.println(a);
+		System.out.println(b);
+		
+	}
+}
